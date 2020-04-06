20e8a65d7dc91b703d7af5beb3ab5c974acca8b7
Victor <Victor.Gamble@outlook.com>
2020-04-06 14:32:56 -0400

    refactored function mapStateToProps to use visibilityFilters

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,7 @@ import React from "react";
 import Todo from "./Todo";
 import { connect } from "react-redux"
 import { getTodos } from "../redux/selectors";
-
+import {getTodosByVisibilityFilter} from '../redux/selectors'
 const TodoList = ({ todos }) => (
   <ul className="todo-list">
     {todos && todos.length
@@ -15,11 +15,8 @@ const TodoList = ({ todos }) => (
 
 
 const mapStateToProps = state => {
-  const { byIds, allIds } = state.todos || {};
-  const todos =
-    allIds && allIds.length
-      ? allIds.map(id => (byIds ? { ...byIds[id], id } : null))
-      : null;
+  const { visibilityFilter } = state;
+  const todos = getTodosByVisibilityFilter(state, visibilityFilter);
   return { todos };
 };
 
